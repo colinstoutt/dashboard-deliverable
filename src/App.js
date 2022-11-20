b77f90d90f31d0fdef7ff371936484e0abfcb216
cps97 <colinstoutt@gmail.com>
2022-11-19 16:23:05 -0800

    apply grid

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Aside from "./components/aside";
+import Reviews from "./components/reviews";
+import Rating from "./components/rating";
+import Analysis from "./components/analysis";
+import Visitors from "./components/visitors";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Aside />
+      <Reviews />
+      <Rating />
+      <Analysis />
+      <Visitors />
+    </div>
+  );
 }
 
 export default App;
